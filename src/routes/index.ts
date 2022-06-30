468bac2cef5147db77657337ebc2c40ff563eeba
joaoAugustoParaty <joaodrow@gmail.com>
2022-06-30 19:55:52 -0300

    URL

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,30 +3,49 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get('/', (_req: Request, _res: Response)=>{
-    let user = {
-        name: 'Joao',
-        lastName: 'Lacerda'
-    };
+    let age: number = 10; 
+    let showOld: boolean = false;
 
-    _res.render('home', {
-        user
+    if(age > 50){
+        showOld = true;
+    }
+       
+    
+
+    _res.render('pages/home', {
+        name: 'Joao',
+        lastName: 'Austo',
+        showOld,
+        products: [
+            {title: 'Produto X', price: 10},
+            {title: 'Produto Y', price: 15},
+            {title: 'Produto W', price: 20},
+
+        ],
+        frasesDoDia: [
+          
+        ]
+
     });
 
 });
 
 router.get('/contato', (_req: Request, _res: Response)=>{
-    _res.send('Formulário de Contato');
+    _res.render('pages/contato');
 
 });
 
 router.get('/sobre', (_req: Request, _res: Response)=>{
-    _res.send('Página institucional sobre a empresa');
+    _res.render('pages/sobre');
 
 });
 
-router.get('/noticias', (_req: Request, _res: Response)=>{
-    _res.send('Lista de notícias');
-
+router.get('/nome', (_req: Request, _res: Response) => {
+    
+    let nome: string = _req.query.nome as string;
+ _res.render('pages/nome',{
+    nome
+ });
 });
 
 export default router;
